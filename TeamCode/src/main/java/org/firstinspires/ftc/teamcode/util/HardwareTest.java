5a3f43959e44feb5c7e2436ff9f78c238935e7d6
Nicolas Silveira <nicsilvc@gmail.com>
2024-05-03 16:43:32 -0700

    Finished stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/HardwareTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/HardwareTest.java
@@ -2,20 +2,23 @@ package org.firstinspires.ftc.teamcode.util;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
+@TeleOp(name = "Hardware Test", group = "Util")
 public class HardwareTest extends LinearOpMode {
     public static String MOTOR_NAME = "driveFL";
+    public static boolean BRAKES_ENABLED = true;
     public static String SERVO_NAME = "";
     DcMotorEx motor;
     Servo servo;
 
     @Override
     public void runOpMode() {
-        motor = hardwareMap.get(DcMotorEx.class, MOTOR_NAME);
-//        servo = hardwareMap.get(Servo.class, SERVO_NAME);
+        initHardware();
 
         telemetry.addData("Status", "Initialized");
         telemetry.addData("Motor to test", MOTOR_NAME);
@@ -30,6 +33,15 @@ public class HardwareTest extends LinearOpMode {
             telemetry.addData("Motor power", motor.getPower());
             telemetry.addData("Motor velocity", motor.getVelocity());
             telemetry.update();
+
+            if (gamepad1.a) initHardware();
         }
+    }
+
+    public void initHardware() {
+        motor = hardwareMap.get(DcMotorEx.class, MOTOR_NAME);
+        motor.setZeroPowerBehavior(BRAKES_ENABLED ? DcMotor.ZeroPowerBehavior.BRAKE : DcMotor.ZeroPowerBehavior.FLOAT);
+
+//        servo = hardwareMap.get(Servo.class, SERVO_NAME);
     }
 }
