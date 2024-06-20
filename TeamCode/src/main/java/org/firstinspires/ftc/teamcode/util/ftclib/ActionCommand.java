f60ab075cb8d95161d1f4dae21b5f38903095343
Anthony <@IDKProd>
2024-06-19 17:01:01 -0700

    Added MathUtil class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ftclib/ActionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ftclib/ActionCommand.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode.util.ftclib;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
