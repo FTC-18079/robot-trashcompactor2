83df0a36cd178801659fdcafd1fa33f8f265f770
Nicolas Silveira <nicsilvc@gmail.com>
2024-08-05 23:18:26 -0700

    Removed telemetry from blue auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueForwardAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueForwardAuto.java
@@ -33,10 +33,6 @@ public class BlueForwardAuto extends LinearOpMode {
             config();
         }
 
-        // Init vision
-        telemetry.addData("Status", "Configuring object detection");
-        telemetry.update();
-
         // Init robot
         Pose2d initialPose = new Pose2d(startingX, startingY, startingH);
         RobotCore robot = new RobotCore(
