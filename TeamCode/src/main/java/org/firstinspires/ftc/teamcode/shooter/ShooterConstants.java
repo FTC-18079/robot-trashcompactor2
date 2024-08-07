11196790b8dfd471d8746162f0b9a40366e8499c
Nicolas Silveira <nicsilvc@gmail.com>
2024-08-06 20:02:38 -0700

    Got servo plate positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/ShooterConstants.java
@@ -4,4 +4,12 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ShooterConstants {
+    public static class Plate {
+        // Left plate poses
+        public double LEFT_PLATE_STOW = 1.0;
+        public double LEFT_PLATE_OUT = 0.0;
+        // Right plate poses
+        public double RIGHT_PLATE_STOW = 0.0;
+        public double RIGHT_PLATE_OUT = 1.0;
+    }
 }
