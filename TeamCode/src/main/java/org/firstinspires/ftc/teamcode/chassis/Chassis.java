3c8101afc67be61735fc8314e11a31766bcb3952
Nicolas Silveira <nicsilvc@gmail.com>
2024-08-10 10:59:29 -0700

    Added chassis driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chassis/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chassis/Chassis.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode.chassis;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.geometry.Pose2d;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RobotCore;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Drawing;
 
 public class Chassis extends SubsystemBase {
     Telemetry telemetry;
@@ -20,29 +20,12 @@ public class Chassis extends SubsystemBase {
 
     /**
      * Drives the robot
-     * @param x the strafe movement input
-     * @param y the forward movement input
-     * @param turn the rotation movement input
+     * @param fwd the forward movement input
+     * @param str the strafe movement input
+     * @param rot the rotation movement input
      */
-    public void setDrivePowers(double x, double y, double turn) {
-//        Vector2d input = new Vector2d(x, -y);
-//        double rotationAmount = -drive.pose.heading.log();
-//
-//        // TODO: fix whatever this is :skull:
-//        if (isFieldCentric) {
-//          if (Global.alliance == BLUE) rotationAmount = rotationAmount - Math.toRadians(90);
-//          else if (Global.alliance == RED) rotationAmount = rotationAmount + Math.toRadians(90);
-//
-//          input = Rotation2d.fromDouble(rotationAmount).times(new Vector2d(input.x, input.y));
-//        }
-//
-//        PoseVelocity2d velocity = new PoseVelocity2d(
-//                new Vector2d(
-//                        input.x,
-//                        input.y
-//                ), -turn
-//        );
-//        this.drive.setDrivePowers(velocity);
+    public void setDrivePowers(double fwd, double str, double rot) {
+        drive.setTeleOpMovementVectors(fwd, str, rot, !isFieldCentric);
     }
 
     public Pose getPoseEstimate() {
@@ -59,15 +42,14 @@ public class Chassis extends SubsystemBase {
 
     // Sets the robot's current heading to be the zero angle
     public void resetHeading() {
-//        this.drive.pose = new Pose2d(
-//                this.drive.pose.position,
-//                Rotation2d.exp(0)
-//        );
+        Pose oldPose = getPoseEstimate();
+        drive.setPose(new Pose(oldPose.getX(), oldPose.getY(), 0));
     }
 
     @Override
     public void periodic() {
         drive.update();
+        Drawing.drawRobot(getPoseEstimate(), "#4CAF50");
         telemetry.addData("Field centric", isFieldCentric);
 //        telemetry.addData("Pose estimate", drive.pose.toString());
     }
